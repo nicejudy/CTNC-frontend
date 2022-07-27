8b0cdcddfa56156a67ad018d9ac9f83756b3ce3b
nicejudy <102686798+nicejudy@users.noreply.github.com>
2022-07-28 05:06:37 +1000

    Update blockchain.ts

--- src/constants/blockchain.ts
+++ src/constants/blockchain.ts
@@ -7,4 +7,5 @@ export enum Networks {
 
 export const DEFAULD_NETWORK = Networks.ETH;
 
-export const RPC_URL = "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+// export const RPC_URL = "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+export const RPC_URL = "https://rpc.cryptotigernode.club";
